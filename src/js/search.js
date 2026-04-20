9aa6d29b59aa19fa2da38bbbc70d06fd919a1171
bukanberuangsr <akhfizpuli@gmail.com>
2026-04-20 23:33:38 +0700

    Update searchUrl to include a variable

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,7 @@
 function performSearch(event) {
     event.preventDefault();
     const query = document.getElementById("search-input").value;
-    const searchURL = "https://www.duckduckgo.com?t=h_&q=" + encodeURIComponent(query);
+    const searchEngine = "https://www.duckduckgo.com?t=h_&q="
+    const searchURL = searchEngine + encodeURIComponent(query);
     window.location.href = searchURL;
 }
