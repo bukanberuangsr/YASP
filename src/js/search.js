9c74e883a3f6a12ef4f2b0faf97702bbd6ae926e
bukanberuangsr <akhfizpuli@gmail.com>
2026-09-16 14:46:49 +0700

    update duckduckgo link

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,7 @@
 function performSearch(event) {
     event.preventDefault();
     const query = document.getElementById("search-input").value;
-    const searchEngine = "https://www.duckduckgo.com?t=h_&q="
+    const searchEngine = "https://duckduckgo.com/?t=ffab&q="
     const searchURL = searchEngine + encodeURIComponent(query);
     window.location.href = searchURL;
 }
